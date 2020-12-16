7c0ecdfe5a917033e8658d5fb39ddce230e40f86
Rafael Pinto <rafael12wq@gmail.com>
2020-12-16 17:10:26 -0300

    Pedido, EstadoPagamento e Pagamento

--- src/main/java/com/course/cursomc/domain/Cliente.java
+++ src/main/java/com/course/cursomc/domain/Cliente.java
@@ -22,7 +22,7 @@ import com.fasterxml.jackson.annotation.JsonManagedReference;
 @Table(name = "tb_cliente")
 public class Cliente implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
@@ -30,14 +30,17 @@ public class Cliente implements Serializable {
 	private String email;
 	private String cpfouCnpj;
 	private Integer tipo;
-	
+
 	@JsonManagedReference
 	@OneToMany(mappedBy = "cliente")
 	private List<Endereco> enderecos = new ArrayList<>();
-	
+
 	@ElementCollection
 	@CollectionTable(name = "TELEFONE")
 	private Set<String> telefones = new HashSet<>();
+
+	@OneToMany(mappedBy = "cliente")
+	private List<Pedido> pedidos = new ArrayList<>();
 
 	public Cliente() {
 
@@ -106,6 +109,14 @@ public class Cliente implements Serializable {
 
 	public void setTelefones(Set<String> telefones) {
 		this.telefones = telefones;
+	}
+
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
 	}
 
 	@Override
---
+++ src/main/java/com/course/cursomc/repositories/PagamentoRepository.java
@@ -0,0 +1,11 @@
+package com.course.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.course.cursomc.domain.Pagamento;
+
+@Repository
+public interface PagamentoRepository extends JpaRepository<Pagamento, Integer>{
+
+}
