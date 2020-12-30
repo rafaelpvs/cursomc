79854741cc06855fd915caa3623cc945a318c199
Rafael Pinto <rafael12wq@gmail.com>
2020-12-30 16:04:11 -0300

    SubTotal e Total do pedido

--- src/main/java/com/course/cursomc/domain/ItemPedido.java
+++ src/main/java/com/course/cursomc/domain/ItemPedido.java
@@ -30,6 +30,10 @@ public class ItemPedido implements Serializable {
 		this.quantidate = quantidate;
 		this.preco = preco;
 	}
+	
+	public double getSubTotal() {
+		return(preco - desconto) * quantidate;
+	}
 
 	@JsonIgnore
 	public Pedido getPedido() {
