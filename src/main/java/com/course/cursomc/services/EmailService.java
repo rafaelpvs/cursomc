5e464219d3d6f1b382637e8783a6699c2828c7b2
Rafael Pinto <rafael12wq@gmail.com>
2021-01-19 11:04:27 -0300

    Email HTML

--- src/main/java/com/course/cursomc/services/EmailService.java
+++ src/main/java/com/course/cursomc/services/EmailService.java
@@ -1,5 +1,7 @@
 package com.course.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.course.cursomc.domain.Pedido;
@@ -8,4 +10,8 @@ public interface EmailService {
 	void sendOrderConfirmationEmail(Pedido obj);
 
 	void sendEmail(SimpleMailMessage msg);
+	
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+	
+	void sendHtmlEmail(MimeMessage msg);
 }
