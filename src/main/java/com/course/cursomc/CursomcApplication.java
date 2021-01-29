bea75721697c438600ec73cfa9363171e7032c6c
Rafael Pinto <rafael12wq@gmail.com>
2021-01-29 16:20:15 -0300

    Salvando primeiro arquivo no S3

--- src/main/java/com/course/cursomc/CursomcApplication.java
+++ src/main/java/com/course/cursomc/CursomcApplication.java
@@ -1,12 +1,16 @@
 package com.course.cursomc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.course.cursomc.services.S3Service;
+
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
-
+	@Autowired
+	private S3Service s3Service;
 	
 
 	public static void main(String[] args) {
@@ -15,7 +19,7 @@ public class CursomcApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		
+		s3Service.uploadFile("C:\\Temp\\blackhole.jpg");
 	}
 
 }
--- src/main/java/com/course/cursomc/resources/AuthResource.java
+++ src/main/java/com/course/cursomc/resources/AuthResource.java
@@ -34,6 +34,7 @@ public class AuthResource {
 		response.addHeader("Authorization", "Bearer " + token);
 		return ResponseEntity.noContent().build();
 	}
+	
 	@RequestMapping(value = "/forgot", method = RequestMethod.POST)
 	public ResponseEntity<Void> forgot(@Valid @RequestBody EmailDTO objDto) {
 		service.sendNewPassword(objDto.getEmail());
