be426ab726de11e9dcb0de9c84edac8295d96380
Rafael Pinto <rafael12wq@gmail.com>
2021-02-03 16:35:28 -0300

    Expondo header location nas respostas

--- src/main/java/com/course/cursomc/CursomcApplication.java
+++ src/main/java/com/course/cursomc/CursomcApplication.java
@@ -1,17 +1,11 @@
 package com.course.cursomc;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.course.cursomc.services.S3Service;
-
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
-	@Autowired
-	private S3Service s3Service;
-	
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
